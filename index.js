2d57171630ee279e93da89dcd1a35d2092225142
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2024-08-25 21:20:57 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import 'dotenv/config.js';
 import express from 'express';
-import { PostgresHelper } from './src/db/postgres/helper.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', async (request, response) => {
-    const results = await PostgresHelper.query('SELECT * FROM users');
-
-    response.send(JSON.stringify(results));
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(process.env.port, () =>
+    console.log(`Server is running on port ${process.env.port}`)
+);
---
+++ src/repositories/postgres/create-user.js
@@ -0,0 +1,17 @@
+import { PostgresHelper } from '../../db/postgres/helper.js';
+export class PostgresCreateUserRepository {
+    async execute(createUserParams) {
+        const results = await PostgresHelper.query(
+            'INSERT INTO users (id, first_name, last_name, email, password) VALUES($1, $2, $3, $4, $5)',
+            [
+                createUserParams.id,
+                createUserParams.firs_name,
+                createUserParams.last_name,
+                createUserParams.email,
+                createUserParams.password,
+            ]
+        );
+
+        return results[0];
+    }
+}
